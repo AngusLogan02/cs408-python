88482df2584627b67826350fe193f7c51ba9a5a1
Angus Logan <angus.logan@baronsview.com>
2024-02-22 15:17:04 +0000

    model cleaned, commented

--- model.py
+++ model.py
@@ -1,94 +1,105 @@
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Input, Conv1D, MaxPool1D, LSTM, Dense, Dropout
 from keras.optimizers import Adam, SGD
 import matplotlib.pyplot as plt
 
+# TODO MAKE ALL QUOTES CONSISTENT
 
-def create_model(input_dim=(23, 256), dropout=False):
-    model = Sequential()
+class ModelTools:
+    def create_model(self, dropout=False) -> Model:
+        """Create the CNN used for Ictal and Pre-Ictal detection.
 
-    dropout = False
+        Parameters
+        ----------
+        dropout : bool
+            Whether or not to enable the dropout layers
 
-    model.add(Input(input_dim))
-    model.add(Conv1D(512, kernel_size=3, activation="relu"))
-    # model.add(Conv1D(512, kernel_size=3, activation="relu"))
-    model.add(MaxPool1D())
-    if dropout:
-        model.add(Dropout(0.25))
-    model.add(Conv1D(256, kernel_size=3, activation="relu"))
-    # model.add(Conv1D(256, kernel_size=3, activation="relu"))
-    model.add(MaxPool1D())
-    if dropout:
-        model.add(Dropout(0.25))
+        Returns
+        -------
+        Keras Model
+            A sequential model made up of CNN, (Dropout), and Dense layers.
 
-    model.add(Conv1D(128, kernel_size=3, activation="relu"))
-    # model.add(Conv1D(128, kernel_size=3, activation="relu"))
-    model.add(MaxPool1D())
-    if dropout:
-        model.add(Dropout(0.25))
+        """
+        model = Sequential()
 
-    model.add(Dense(256, activation="relu"))
-    if dropout:
-        model.add(Dropout(0.5))
-    model.add(Dense(1, activation="sigmoid"))
+        model.add(Input(23, 256))
+        model.add(Conv1D(512, kernel_size=3, activation="relu"))
+        model.add(MaxPool1D())
+        if dropout:
+            model.add(Dropout(0.25))
+        model.add(Conv1D(256, kernel_size=3, activation="relu"))
+        model.add(MaxPool1D())
+        if dropout:
+            model.add(Dropout(0.25))
 
-    return model
+        model.add(Conv1D(128, kernel_size=3, activation="relu"))
+        model.add(MaxPool1D())
+        if dropout:
+            model.add(Dropout(0.25))
 
+        model.add(Dense(256, activation="relu"))
+        if dropout:
+            model.add(Dropout(0.5))
+        model.add(Dense(1, activation="sigmoid"))
 
-def create_large_model(input_dim=(23, 256)):
-    model = Sequential()
-
-    model.add(Input(input_dim))
-    # model.add(Conv1D(1024, kernel_size=3, activation="relu"))
-    # model.add(MaxPool1D())
-    # model.add(Conv1D(512, kernel_size=3, activation="relu"))
-    # model.add(Dropout(0.2))
-    # model.add(Conv1D(256, kernel_size=3, activation="relu"))
-    # model.add(Dropout(0.2))
-    # model.add(Conv1D(128, kernel_size=3, activation="relu"))
-    # model.add(Dropout(0.2))
-    # # model.add(LSTM(64, activation="tanh"))
-    # model.add(Dense(256, activation="relu"))
-    model.add(Dense(64, activation="relu"))
-    model.add(Dense(1, activation="sigmoid"))
-
-    return model
+        return model
 
 
-def create_recurrent_model():
-    model = Sequential()
+    def compile_model(self, model) -> Model:
+        """Compiles a specified model.
 
-    model.add(LSTM(256, activation="tanh", return_sequences=True, input_shape=(23, 7)))
-    model.add(LSTM(512, activation="tanh", return_sequences=True))
-    model.add(LSTM(256, activation="tanh"))
-    model.add(Dense(128, activation="sigmoid"))
-    model.add(Dense(1, activation="sigmoid"))
+        Compiles the given model with the Adam optimiser and the binary
+        cross-entropy loss function.
+        
+        Parameters
+        ----------
+        model : Keras Model
 
-    return model
+        Returns
+        -------
+        Compiled Keras Model
+        
+        """
+        model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+        return model
 
-def compile_model(model):
-    model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
-    return model
 
-def accuracy_loss_plot(history, combined=False):
-    if not combined:
-        history = history.history
-    # Plotting accuracy and loss over epochs
-    plt.figure(figsize=(12, 4))
+    def accuracy_loss_plot(self, history) -> None:
+        """A utility function to simplify graphing a model's performance.
 
-    # Plotting accuracy
-    plt.subplot(1, 2, 1)
-    plt.plot(history['accuracy'])
-    plt.title('Model Accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
+        Notes
+        -----
+        Could be improved to take in a list of histories, and iterate over
+        them, combining them into one graph. This would enable the easy
+        graphing of a model trained with multiple steps.
+        
+        Parameters
+        ----------
+        history : History
+            The output of the model.fit method.
 
-    # Plotting loss
-    plt.subplot(1, 2, 2)
-    plt.plot(history['loss'])
-    plt.title('Model Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
+        Returns
+        -------
+        Nothing, but displays a line graph of the model's accuracy at each
+        training epoch.
 
-    plt.tight_layout()
-    plt.show()
+        """
+        # Plotting accuracy and loss over epochs
+        plt.figure(figsize=(12, 4))
+
+        # Plotting accuracy
+        plt.subplot(1, 2, 1)
+        plt.plot(history.history["accuracy"])
+        plt.title("Model Accuracy")
+        plt.xlabel("Epochs")
+        plt.ylabel("Accuracy")
+
+        # Plotting loss
+        plt.subplot(1, 2, 2)
+        plt.plot(history.history["loss"])
+        plt.title("Model Loss")
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+
+        plt.tight_layout()
+        plt.show()
